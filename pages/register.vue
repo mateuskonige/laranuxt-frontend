7a1367bfea5e2af2a9d7cfa9f770060b6f1431b0
mateuskonige <mateuskonige@gmail.com>
2022-01-06 08:19:39 -0300

    validation fix

--- pages/register.vue
+++ pages/register.vue
@@ -6,13 +6,26 @@
     <form @submit.prevent="submit">
       <div class="mb-3">
         <label class="form-label">Full name</label>
-        <input v-model.trim="form.name" type="text" class="form-control" placeholder="Enter your name" />
-        <small class="alert-danger" v-if="errors.name">{{ errors.name[0] }}</small>
+        <input
+          v-model.trim="form.name"
+          type="text"
+          class="form-control"
+          placeholder="Enter your name"
+          autofocus
+        />
+        <small class="alert-danger" v-if="errors.name">{{
+          errors.name[0]
+        }}</small>
       </div>
 
       <div class="mb-3">
         <label class="form-label">Email address</label>
-        <input v-model.trim="form.email" type="email" class="form-control" placeholder="Enter e-mail" />
+        <input
+          v-model.trim="form.email"
+          type="email"
+          class="form-control"
+          placeholder="Enter e-mail"
+        />
         <small class="alert-danger" v-if="errors.email">{{ errors.email[0] }}</small>
       </div>
 
@@ -26,7 +39,7 @@
         />
         <small class="alert-danger" v-if="errors.password">{{ errors.password[0] }}</small>
       </div>
-      
+
       <button type="submit" class="btn btn-primary">Register</button>
     </form>
     <br />
@@ -39,23 +52,28 @@ export default {
   data() {
     return {
       form: {
-        name: '',
-        email: '',
-        password: ''
-      }
-    }
+        name: "",
+        email: "",
+        password: "",
+      },
+    };
   },
   methods: {
     async submit() {
-      await this.$axios.$post('register', this.form)
-      await this.$auth.loginWith('local', {
-        data: {
-          email: this.form.email,
-          password: this.form.password
-        }
-      })
-      this.$router.push('/')
-    }
-  }
+      try {
+        await this.$axios.$post("register", this.form);
+
+        let response = await this.$auth.loginWith("local", {
+          data: {
+            email: this.form.email,
+            password: this.form.password,
+          },
+        });
+        this.$router.push("/");
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
 };
 </script>
